d3d28b38054a1e70e85de57d6bbee664db2ad935
yhusoonpoint <allinsonadebambo@gmail.com>
2024-02-20 21:34:40 +0000

    Update Folder_Tree.py
    Saves to a file instead

--- Folder_Tree.py
+++ Folder_Tree.py
@@ -7,18 +7,20 @@ def pick_folder():
     root.withdraw()
     folder_path = filedialog.askdirectory()
     if folder_path:
-        print("****** " + folder_path + " ******")
-        list_files_and_folders(folder_path)
+        output_file = "tree.txt"
+        with open(output_file, "w") as f:
+            f.write("****** " + folder_path + " ******\n")
+            list_files_and_folders(folder_path, f)
 
-def list_files_and_folders(folder_path, indent=""):
+def list_files_and_folders(folder_path, output_file, indent=""):
     root_folder_name = os.path.basename(folder_path)
-    print(indent + root_folder_name + "/")
+    output_file.write(indent + root_folder_name + "/\n")
     indent += "   "
     for root_folder, sub_folders, files in os.walk(folder_path):
         for sub_folder in sorted(sub_folders):
-            list_files_and_folders(os.path.join(root_folder, sub_folder), indent)
+            list_files_and_folders(os.path.join(root_folder, sub_folder), output_file, indent)
         for file in sorted(files):
-            print(indent + file)
+            output_file.write(indent + file + "\n")
 
 if __name__ == "__main__":
     pick_folder()
